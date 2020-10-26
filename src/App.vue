2b13d493d0f7db4a029fd9303f4b313cd7d68551
Thomas Maitret Richard <thomas.maitret@laposte.net>
2020-10-26 01:02:51 +0100

    init repo

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <SkipLink />
+  <main class="container" id="maincontent">
+    <Posts />
+  </main>
+  <ThemeSwitcher />
+</template>
+
+<script>
+import ThemeSwitcher from "./components/ThemeSwitcherComponent.vue";
+import SkipLink from "./components/SkipLinkComponent.vue";
+import Posts from "./components/PostsComponent.vue";
+
+export default {
+  name: "App",
+  components: { ThemeSwitcher, SkipLink, Posts },
+};
+</script>
+
+<style scoped>
+.container {
+  padding: 0 1.5rem;
+  margin-right: auto;
+  margin-left: auto;
+  max-width: 960px;
+}
+</style>
---
+++ src/components/ThemeSwitcherComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <button
+    class="theme-switcher"
+    aria-label="Change theme"
+    @click="switchTheme"
+  ></button>
+</template>
+
+<script>
+export default {
+  name: "ThemeSwitcher",
+  methods: {
+    switchTheme() {
+      const theme =
+        document.body.getAttribute("data-theme") === "dark" ? "light" : "dark";
+      document.body.setAttribute("data-theme", theme);
+      localStorage.setItem("theme", theme);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.theme-switcher {
+  position: fixed;
+  right: 1.5rem;
+  bottom: 1.5rem;
+  width: 50px;
+  height: 50px;
+  background-color: var(--primary-color);
+  border-radius: 30px;
+  cursor: pointer;
+  border: none;
+}
+</style>
